47d2afe40513f8f4de908cd5baa01c0ffc919588
Vincent Moittié <vincent.moittie@cji.paris>
2022-08-12 11:40:07 +0200

    Fix bad build name

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: path.resolve(__dirname, 'src/index.js'),
   output: {
     path: path.resolve(__dirname, './dist'),
-    filename: 'leto-plugin-core.js',
+    filename: 'leto-modelizer-plugin-core.js',
     libraryTarget: 'commonjs',
   },
   resolve: {
