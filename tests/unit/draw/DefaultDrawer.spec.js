12e35d4dea0961f496eea92b43381b429cd842b7
Hundraw <chauvel.dimitri@posteo.net>
2022-08-19 10:40:56 +0200

    Drawing in container and resize it

--- tests/unit/draw/DefaultDrawer.spec.js
+++ tests/unit/draw/DefaultDrawer.spec.js
@@ -7,15 +7,16 @@ describe('Test Class: DefaultDrawer()', () => {
     it('Test default constructor', () => {
       const drawer = new DefaultDrawer();
       expect(drawer.resources).toBeNull();
+      expect(drawer.rootId).toEqual('root');
       expect(drawer.d3).not.toBeNull();
     });
 
     it('Test passing value in constructor', () => {
-      const drawer = new DefaultDrawer('parentId', 'resources');
-      expect(drawer.parentId).toEqual('parentId');
+      const drawer = new DefaultDrawer('resources', 'rootId');
       expect(drawer.resources).toEqual('resources');
+      expect(drawer.rootId).toEqual('rootId');
       expect(drawer.d3).not.toBeNull();
-      expect(drawer.margin).toEqual(10);
+      expect(drawer.margin).toEqual(6);
       expect(drawer.actions).not.toBeNull();
     });
   });
@@ -25,14 +26,23 @@ describe('Test Class: DefaultDrawer()', () => {
     let actionCallBack;
 
     beforeEach(() => {
-      actionCallBack = { data: {} };
+      actionCallBack = { data: [], attr: [], html: [] };
       drawer = new DefaultDrawer();
       drawer.d3 = mockD3(jest);
+      drawer.resources = { models: { test0: 'testModel0', test1: 'testModel1' }, icons: { test: 'testIcon' } };
       drawer.initializeComponents = jest.fn();
       drawer.setViewPortAction = jest.fn();
       drawer.setComponentAction = jest.fn();
       drawer.d3.data = jest.fn((components, data) => {
-        actionCallBack.data = data;
+        actionCallBack.data.push({ data });
+        return drawer.d3;
+      });
+      drawer.d3.attr = jest.fn((name, callback) => {
+        actionCallBack.attr.push({ name, callback });
+        return drawer.d3;
+      });
+      drawer.d3.html = jest.fn((callback) => {
+        actionCallBack.html.push({ callback });
         return drawer.d3;
       });
     });
@@ -43,7 +53,7 @@ describe('Test Class: DefaultDrawer()', () => {
       expect(drawer.d3.select).toBeCalledTimes(2);
       expect(drawer.d3.selectAll).toBeCalledTimes(1);
       expect(drawer.d3.data).toBeCalledTimes(1);
-      expect(actionCallBack.data({ id: 'id' })).toEqual('id');
+      expect(actionCallBack.data[0].data({ id: 'id' })).toEqual('id');
       expect(drawer.initializeComponents).toBeCalledTimes(1);
       expect(drawer.setViewPortAction).toBeCalledTimes(1);
       expect(drawer.setComponentAction).not.toBeCalled();
@@ -52,102 +62,175 @@ describe('Test Class: DefaultDrawer()', () => {
     it('Draw should call the internal methods', () => {
       drawer.drawCustomModel = jest.fn();
       drawer.draw([{
+        children: [],
         definition: { model: 'DefaultModel' },
       }, {
+        children: [],
         definition: { model: null },
       }, {
+        children: [],
         definition: { model: 'CustomModel' },
       }, {
+        children: [{
+          children: [],
+          definition: { model: 'CustomModel' },
+        }],
         definition: { model: 'bad' },
-      }]);
+      }], 'parent');
 
-      expect(drawer.d3.select).toBeCalledTimes(2);
-      expect(drawer.d3.selectAll).toBeCalledTimes(1);
-      expect(drawer.d3.data).toBeCalledTimes(1);
-      expect(actionCallBack.data({ id: 'id' })).toEqual('id');
-      expect(drawer.initializeComponents).toBeCalledTimes(1);
-      expect(drawer.setViewPortAction).toBeCalledTimes(1);
-      expect(drawer.setComponentAction).toBeCalledTimes(4);
+      expect(drawer.d3.select).toBeCalledTimes(7);
+      expect(drawer.d3.selectAll).toBeCalledTimes(7);
+      expect(drawer.d3.data).toBeCalledTimes(2);
+      expect(actionCallBack.data[0].data({ id: 'id0' })).toEqual('id0');
+      expect(actionCallBack.data[1].data({ id: 'id1' })).toEqual('id1');
+      expect(drawer.initializeComponents).toBeCalledTimes(2);
+      expect(drawer.setViewPortAction).toBeCalledTimes(2);
+      expect(drawer.setComponentAction).toBeCalledTimes(5);
+
+      expect(drawer.d3.attr).toBeCalledTimes(4);
+      expect(actionCallBack.attr[0].name).toEqual('id');
+      expect(actionCallBack.attr[0].callback({ id: 'idTest0' })).toEqual('idTest0');
+      expect(actionCallBack.attr[1].name).toEqual('class');
+      expect(actionCallBack.attr[1].callback({ definition: { model: 'test0' } })).toEqual('parent component component-test0');
+      expect(actionCallBack.attr[2].name).toEqual('id');
+      expect(actionCallBack.attr[2].callback({ id: 'idTest1' })).toEqual('idTest1');
+      expect(actionCallBack.attr[3].name).toEqual('class');
+      expect(actionCallBack.attr[3].callback({ definition: { model: 'test1' } })).toEqual('root component component-test1');
+
+      expect(drawer.d3.html).toBeCalledTimes(3);
+      expect(actionCallBack.html[0].callback({ definition: { model: 'test0' } })).toEqual('testModel0');
+      expect(actionCallBack.html[1].callback({ definition: { icon: 'test' } })).toEqual('testIcon');
+      expect(actionCallBack.html[2].callback({ definition: { model: 'test1' } })).toEqual('testModel1');
     });
   });
 
   describe('Test method: initializeComponents', () => {
     let drawer;
+    let d3Element;
+    let components;
     let component00;
     let component01;
     let component02;
-    let d3Element;
     let actionCallBack;
-
     beforeEach(() => {
-      actionCallBack = { attr: {}, html: {} };
-      document.body.innerHTML = '<svg id="viewport"></svg>';
-      drawer = new DefaultDrawer('#root', { models: { DefaultModel: 'test' } });
-      drawer.storeComponentSize = jest.fn();
-      drawer.setComponentPosition = jest.fn();
-      component00 = { drawOption: { width: 10, height: 10 }, definition: { model: 'DefaultModel' } };
-      component01 = { drawOption: null, definition: { model: 'template' } };
-      component02 = { drawOption: new ComponentDrawOption(), definition: { model: 'template' } };
-      d3Element = mockD3(jest);
+      actionCallBack = { attr: [], html: {} };
+      drawer = new DefaultDrawer();
       drawer.d3 = mockD3(jest);
-      d3Element.each = jest.fn((callback) => {
-        actionCallBack.each = callback;
-        return d3Element;
+      d3Element = mockD3(jest);
+      drawer.pack = jest.fn().mockReturnValue({
+        width: 1,
+        height: 1,
+        items: [
+          { item: { id: 'a' }, x: 1, y: 2 },
+          { item: { id: 'b' }, x: 3, y: 4 },
+          { item: { id: 'c' }, x: 5, y: 6 },
+        ],
       });
+      d3Element.drawOption = {
+        x: 1,
+        y: 2,
+        width: 3,
+        height: 4,
+      };
+      component00 = { id: 'a', drawOption: null };
+      component01 = { id: 'b', drawOption: null };
+      component02 = {
+        id: 'c',
+        drawOption: {
+          x: 1,
+          y: 2,
+          width: 0,
+          height: 0,
+        },
+      };
+      components = [component00, component01, component02];
       d3Element.attr = jest.fn((name, callback) => {
-        actionCallBack.attr[name] = callback;
-        return d3Element;
-      });
-      d3Element.html = jest.fn((callback) => {
-        actionCallBack.html = callback;
+        actionCallBack.attr.push({ name, callback });
         return d3Element;
       });
     });
 
     it('Should call every internal methods and set a drawOption if null', () => {
-      drawer.storeComponentSize.mockImplementation((list, component) => {
-        component.drawOption.width = 10;
-        component.drawOption.height = 10;
+      drawer.initializeComponents(d3Element, components, 'parentId');
+
+      expect(components[0].drawOption).not.toBeNull();
+      expect(components[0].drawOption.x).toEqual(7);
+      expect(components[0].drawOption.y).toEqual(8);
+
+      expect(components[1].drawOption).not.toBeNull();
+      expect(components[1].drawOption.x).toEqual(9);
+      expect(components[1].drawOption.y).toEqual(10);
+
+      expect(components[2].drawOption).not.toBeNull();
+      expect(components[2].drawOption.x).toEqual(1);
+      expect(components[2].drawOption.y).toEqual(2);
+
+      expect(d3Element.attr).toBeCalledTimes(6);
+      expect(actionCallBack.attr[0].name).toEqual('x');
+      expect(actionCallBack.attr[0].callback({ drawOption: { x: 1 } })).toEqual(1);
+      expect(actionCallBack.attr[1].name).toEqual('y');
+      expect(actionCallBack.attr[1].callback({ drawOption: { y: 2 } })).toEqual(2);
+      expect(actionCallBack.attr[2].name).toEqual('width');
+      expect(actionCallBack.attr[2].callback({ drawOption: { width: 3 } })).toEqual(3);
+      expect(actionCallBack.attr[3].name).toEqual('height');
+      expect(actionCallBack.attr[3].callback({ drawOption: { height: 4 } })).toEqual(4);
+      expect(actionCallBack.attr[4].name).toEqual('width');
+      expect(actionCallBack.attr[4].callback).toEqual('100%');
+      expect(actionCallBack.attr[5].name).toEqual('height');
+      expect(actionCallBack.attr[5].callback).toEqual('100%');
+
+      expect(d3Element.select).toBeCalledTimes(1);
+      expect(d3Element.select.mock.calls[0][0]).toBe('.template');
+    });
+  });
+
+  describe('Test method: setContainerSize', () => {
+    let drawer;
+    let d3Element;
+    let components;
+    let component00;
+    let component01;
+    let component02;
+    beforeEach(() => {
+      drawer = new DefaultDrawer();
+      drawer.d3 = mockD3(jest);
+      d3Element = mockD3(jest);
+      drawer.pack = jest.fn().mockReturnValue({
+        width: 1,
+        height: 1,
+        items: [
+          { item: { id: 'a' }, x: 1, y: 2 },
+          { item: { id: 'b' }, x: 3, y: 4 },
+          { item: { id: 'c' }, x: 5, y: 6 },
+        ],
       });
-      drawer.initializeComponents([component00, component01], d3Element);
-      actionCallBack.each(null, 0, [0]);
-      actionCallBack.each(null, 1, [0]);
-
-      expect(d3Element.enter).toBeCalledTimes(1);
-      expect(d3Element.append).toBeCalledTimes(1);
-      expect(d3Element.append.mock.calls[0][0]).toBe('svg');
-      expect(d3Element.exit).toBeCalledTimes(1);
-      expect(d3Element.remove).toBeCalledTimes(1);
-
-      expect(actionCallBack.attr.id({ id: 'id' })).toEqual('id');
-      expect(actionCallBack.attr.class({ definition: { model: 'DefaultModel' } }))
-        .toEqual('component component-DefaultModel');
-      expect(actionCallBack.html({ definition: { model: 'DefaultModel' } }))
-        .toEqual('test');
-
-      expect(d3Element.each).toBeCalledTimes(1);
-
-      expect(component01.drawOption).not.toEqual(null);
-
-      expect(d3Element.attr).toBeCalled();
-      expect(d3Element.html).toBeCalled();
-
-      expect(drawer.storeComponentSize).toBeCalled();
-      expect(drawer.setComponentPosition).toBeCalledTimes(1);
+      d3Element.drawOption = {
+        x: 1,
+        y: 2,
+        width: 3,
+        height: 4,
+      };
+      component00 = { id: 'a', drawOption: { width: 1, height: 1 } };
+      component01 = { id: 'b', drawOption: { width: 1, height: 1 } };
+      component02 = { id: 'c', drawOption: { width: 1, height: 1 } };
+      components = [component00, component01, component02];
     });
 
-    it('Should not call setComponentPosition method', () => {
-      drawer.storeComponentSize.mockImplementation((list, component) => {
-        component.drawOption.width = 1;
-        component.drawOption.height = 1;
-      });
-      drawer.initializeComponents([component02], d3Element);
-      expect(drawer.setComponentPosition).toBeCalledTimes(1);
+    it('Should call d3 methods to setup containers sizes', () => {
+      drawer.setContainerSize(components, 'parentId');
+
+      expect(drawer.d3.attr).toBeCalledTimes(5);
+      expect(drawer.d3.attr.mock.calls[0][0]).toBe('y');
+      expect(drawer.d3.attr.mock.calls[1][0]).toBe('width');
+      expect(drawer.d3.attr.mock.calls[2][0]).toBe('height');
+      expect(drawer.d3.attr.mock.calls[3][0]).toBe('width');
+      expect(drawer.d3.attr.mock.calls[4][0]).toBe('height');
     });
   });
 
   describe('Test method: setComponentPosition', () => {
     let drawer;
+    let positions;
     let component01;
     let component02;
     let component03;
@@ -155,8 +238,13 @@ describe('Test Class: DefaultDrawer()', () => {
 
     beforeEach(() => {
       drawer = new DefaultDrawer();
+      positions = [
+        { item: { id: 'a' }, x: 1, y: 2 },
+        { item: { id: 'b' }, x: 3, y: 4 },
+        { item: { id: 'c' }, x: 5, y: 6 },
+      ];
       component01 = {
-        id: 'id01',
+        id: 'a',
         drawOption: new ComponentDrawOption({
           x: 1,
           y: 1,
@@ -165,7 +253,7 @@ describe('Test Class: DefaultDrawer()', () => {
         }),
       };
       component02 = {
-        id: 'id02',
+        id: 'b',
         drawOption: new ComponentDrawOption({
           x: 1,
           y: 1,
@@ -174,7 +262,7 @@ describe('Test Class: DefaultDrawer()', () => {
         }),
       };
       component03 = {
-        id: 'id03',
+        id: 'c',
         drawOption: new ComponentDrawOption({
           x: 1,
           y: 1,
@@ -183,49 +271,40 @@ describe('Test Class: DefaultDrawer()', () => {
         }),
       };
       components = [component01, component02, component03];
-      drawer.pack = jest.fn().mockReturnValue({
-        items: [
-          { x: 1 + drawer.margin, y: 100 + drawer.margin, item: { id: 'id01' } },
-          { x: 1 + drawer.margin * 2, y: 100 + drawer.margin * 2, item: { id: 'id02' } },
-          { x: 1 + drawer.margin * 3, y: 100 + drawer.margin * 3, item: { id: 'id03' } },
-        ],
-      });
     });
 
     it('Should set the good positions to components.drawOption', () => {
-      drawer.setComponentPosition(components);
+      drawer.setComponentPosition(components, positions);
 
-      expect(drawer.pack).toBeCalledTimes(1);
-
-      expect(components[0].drawOption.x).toEqual(21);
-      expect(components[0].drawOption.y).toEqual(120);
-      expect(components[1].drawOption.x).toEqual(31);
-      expect(components[1].drawOption.y).toEqual(130);
-      expect(components[2].drawOption.x).toEqual(41);
-      expect(components[2].drawOption.y).toEqual(140);
+      expect(components[0].drawOption.x).toEqual(7);
+      expect(components[0].drawOption.y).toEqual(8);
+      expect(components[1].drawOption.x).toEqual(9);
+      expect(components[1].drawOption.y).toEqual(10);
+      expect(components[2].drawOption.x).toEqual(11);
+      expect(components[2].drawOption.y).toEqual(12);
     });
   });
 
   describe('Test method: storeComponentSize', () => {
     let drawer;
-    let component;
+    let components;
 
     beforeEach(() => {
       drawer = new DefaultDrawer();
       drawer.getComponentSize = jest.fn().mockReturnValue({ width: 1, height: 2 });
-      component = {
+      components = [{
         drawOption: {
           width: null,
           height: null,
         },
-      };
+      }];
     });
 
     it('Should set the component.drawOption width and height', () => {
-      drawer.storeComponentSize(0, component);
-      expect(drawer.getComponentSize).toBeCalledWith(0);
-      expect(component.drawOption.width).toEqual(1);
-      expect(component.drawOption.height).toEqual(2);
+      drawer.storeComponentSize(components);
+      expect(drawer.getComponentSize).toBeCalled();
+      expect(components[0].drawOption.width).toEqual(1);
+      expect(components[0].drawOption.height).toEqual(2);
     });
   });
 
@@ -255,7 +334,7 @@ describe('Test Class: DefaultDrawer()', () => {
 
     beforeEach(() => {
       actionCallBack = { attr: {}, html: {}, text: [] };
-      drawer = new DefaultDrawer('#root', { icons: { DefaultIcon: 'test' } });
+      drawer = new DefaultDrawer({ icons: { DefaultIcon: 'test' } }, 'root');
       d3Element = mockD3(jest);
       d3Element.attr = jest.fn((name, callback) => {
         actionCallBack.attr[name] = callback;
@@ -274,10 +353,45 @@ describe('Test Class: DefaultDrawer()', () => {
     it('drawDefaultModel should setup default d3Element', () => {
       drawer.drawDefaultModel(d3Element);
 
-      expect(d3Element.attr).toBeCalledTimes(3);
-      expect(d3Element.attr.mock.calls[0][0]).toBe('x');
-      expect(d3Element.attr.mock.calls[1][0]).toBe('y');
-      expect(d3Element.attr.mock.calls[2][0]).toBe('cursor');
+      expect(d3Element.select).toBeCalledTimes(3);
+      expect(d3Element.select.mock.calls[0][0]).toBe('.component-name');
+      expect(d3Element.select.mock.calls[1][0]).toBe('.component-type');
+      expect(d3Element.select.mock.calls[2][0]).toBe('.component-icon');
+
+      expect(d3Element.html).toBeCalledTimes(1);
+      expect(d3Element.text).toBeCalledTimes(2);
+      expect(actionCallBack.text[0]({ name: 'test-name' })).toEqual('test-name');
+      expect(actionCallBack.text[1]({ definition: { type: 'test-type' } })).toEqual('test-type');
+
+      expect(actionCallBack.html({ definition: { icon: 'DefaultIcon' } })).toEqual('test');
+    });
+  });
+
+  describe('Test method: drawDefaultContainer', () => {
+    let drawer;
+    let d3Element;
+    let actionCallBack;
+
+    beforeEach(() => {
+      actionCallBack = { attr: {}, html: {}, text: [] };
+      drawer = new DefaultDrawer({ icons: { DefaultIcon: 'test' } }, 'root');
+      d3Element = mockD3(jest);
+      d3Element.attr = jest.fn((name, callback) => {
+        actionCallBack.attr[name] = callback;
+        return d3Element;
+      });
+      d3Element.text = jest.fn((callback) => {
+        actionCallBack.text.push(callback);
+        return d3Element;
+      });
+      d3Element.html = jest.fn((callback) => {
+        actionCallBack.html = callback;
+        return d3Element;
+      });
+    });
+
+    it('drawDefaultModel should setup default d3Element', () => {
+      drawer.drawDefaultContainer(d3Element);
 
       expect(d3Element.select).toBeCalledTimes(3);
       expect(d3Element.select.mock.calls[0][0]).toBe('.component-name');
@@ -289,8 +403,6 @@ describe('Test Class: DefaultDrawer()', () => {
       expect(actionCallBack.text[0]({ name: 'test-name' })).toEqual('test-name');
       expect(actionCallBack.text[1]({ definition: { type: 'test-type' } })).toEqual('test-type');
 
-      expect(actionCallBack.attr.x({ drawOption: { x: 1 } })).toEqual(1);
-      expect(actionCallBack.attr.y({ drawOption: { y: 1 } })).toEqual(1);
       expect(actionCallBack.html({ definition: { icon: 'DefaultIcon' } })).toEqual('test');
     });
   });
