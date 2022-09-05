b46bca2a95444123ce27f4c95d9222631b7b2caf
Vincent Moittié <vincent.moittie@cji.paris>
2022-09-05 16:41:21 +0200

    Setup demo

---
+++ demo/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <div>
+      <h1>Demo of default drawing in leto-modelizer</h1>
+    </div>
+    <div>
+      <svg id='root' width='80vw' height='80vh' style='border: 1px solid black'></svg>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { onMounted } from 'vue';
+import { DefaultDrawer } from 'leto-modelizer-plugin-core';
+import resources from './assets/resources';
+import plugin from './assets/data';
+
+const drawer = new DefaultDrawer(resources, 'root');
+
+onMounted(() => {
+  drawer.draw(plugin.components);
+});
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+#viewport {
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+}
+</style>
