2c6a82d5de5f286ecb6332d90b82025d8432c390
Hundraw <chauvel.dimitri@posteo.net>
2022-11-07 10:15:36 +0100

    Update demo

--- demo/src/assets/data.js
+++ demo/src/assets/data.js
@@ -3,7 +3,7 @@ import {
   ComponentAttribute,
   ComponentAttributeDefinition,
   ComponentDefinition,
-  ComponentLink,
+  ComponentLink, ComponentLinkDefinition,
 } from 'leto-modelizer-plugin-core';
 
 // Component attribute Definitions
@@ -23,6 +23,7 @@ const networkAttributeDefinition = new ComponentAttributeDefinition({
   type: 'Reference',
   containerRef: 'network',
 });
+
 const ipAttributeDefinition = new ComponentAttributeDefinition({
   name: 'ip',
   type: 'String',
@@ -30,10 +31,24 @@ const ipAttributeDefinition = new ComponentAttributeDefinition({
     regex: '/[0-9]{1-3}\\.[0-9]{1-3}\\.[0-9]{1-3}\\.[0-9]{1-3}',
   },
 });
+
 const userAttributeDefinition = new ComponentAttributeDefinition({
   name: 'fullname',
   type: 'String',
 });
+
+const laptopLinkAttributeDefinition = new ComponentAttributeDefinition({
+  name: 'laptop_link',
+  type: 'Link',
+  linkRef: 'laptop',
+})
+
+
+const networkLinkAttributeDefinition = new ComponentAttributeDefinition({
+  name: 'network_link',
+  type: 'Link',
+  linkRef: 'network',
+})
 
 // Component Definitions
 const networkDefinition = new ComponentDefinition({
@@ -51,7 +66,12 @@ const serverDefinition = new ComponentDefinition({
   icon: 'server',
   model: 'DefaultModel',
   parentTypes: ['network'],
-  definedAttributes: [nameAttributeDefinition, networkAttributeDefinition, ipAttributeDefinition],
+  definedAttributes: [
+    nameAttributeDefinition,
+    networkAttributeDefinition,
+    ipAttributeDefinition,
+    laptopLinkAttributeDefinition,
+  ],
   isContainer: false,
 });
 
@@ -60,7 +80,12 @@ const laptopDefinition = new ComponentDefinition({
   icon: 'laptop',
   model: 'DefaultModel',
   parentTypes: [],
-  definedAttributes: [nameAttributeDefinition, ipAttributeDefinition, userAttributeDefinition],
+  definedAttributes: [
+    nameAttributeDefinition,
+    ipAttributeDefinition,
+    userAttributeDefinition,
+    networkLinkAttributeDefinition,
+  ],
   isContainer: false,
 });
 
@@ -203,14 +228,30 @@ const dmz2 = new Component({
   children: [],
 });
 
+const linkDefinition1 = new ComponentLinkDefinition({
+  attributeRef: 'network_link',
+  sourceRef: 'laptop',
+  targetRef: 'network',
+  type: 'Default',
+});
+
+const linkDefinition2 = new ComponentLinkDefinition({
+  attributeRef: 'laptop_link',
+  sourceRef: 'server',
+  targetRef: 'laptop',
+  type: 'Default',
+});
+
 const link1 = new ComponentLink({
-  source: 'network1',
-  target: 'internal1',
+  source: 'internal1',
+  target: 'network1',
+  definition: linkDefinition1,
 });
 
 const link2 = new ComponentLink({
   source: 'server1',
   target: 'external1',
+  definition: linkDefinition2,
 });
 
 export default {
@@ -219,5 +260,6 @@ export default {
     links: [],
   },
   components: [internalLaptop, dmz1, dmz2, externalLaptop],
-  links: [link1, link2]
+  links: [link1, link2],
+  linksDefinition: [linkDefinition1, linkDefinition2],
 };
