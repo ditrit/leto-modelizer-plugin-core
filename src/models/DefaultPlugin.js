9e319f69b08c0275556e3d0b5140c35fb37f338e
Vincent Moittie <moittie.vincent@gmail.com>
2022-11-09 13:59:37 +0100

    Add default plugin model

---
+++ src/models/DefaultPlugin.js
@@ -0,0 +1,112 @@
+import DefaultData from './DefaultData';
+import DefaultDrawer from '../draw/DefaultDrawer';
+import DefaultMetadata from '../metadata/DefaultMetadata';
+import DefaultParser from '../parser/DefaultParser';
+import DefaultRender from '../render/DefaultRender';
+
+/**
+ * Default plugin structure.
+ */
+class DefaultPlugin {
+  /**
+   * Default constructor.
+   * @param {DefaultData} props.pluginData - Plugin data storage.
+   * @param {DefaultDrawer} props.pluginDrawer - Plugin drawer.
+   * @param {DefaultMetadata} props.pluginMetadata - Plugin metadata.
+   * @param {DefaultParser} props.pluginParser - Plugin parser.
+   * @param {DefaultRender} props.pluginRenderer - Plugin renderer.
+   */
+  constructor(props = {
+    pluginData: null,
+    pluginDrawer: null,
+    pluginMetadata: null,
+    pluginParser: null,
+    pluginRenderer: null,
+  }) {
+    /**
+     * Plugin data storage.
+     * @type {DefaultData}
+     */
+    this.data = props.pluginData || new DefaultData();
+    /**
+     * Plugin drawer.
+     * @type {DefaultDrawer}
+     * @private
+     */
+    this.__drawer = props.pluginDrawer || new DefaultDrawer(this.data);
+    /**
+     * Plugin metadata.
+     * @type {DefaultMetadata}
+     * @private
+     */
+    this.__metadata = props.pluginMetadata || new DefaultMetadata(this.data);
+    /**
+     * Plugin parser.
+     * @type {DefaultParser}
+     * @private
+     */
+    this.__parser = props.pluginParser || new DefaultParser(this.data);
+    /**
+     * Plugin renderer.
+     * @type {DefaultRender}
+     * @private
+     */
+    this.__renderer = props.pluginRenderer || new DefaultRender(this.data);
+  }
+
+  /**
+   * Init method, to call once before all plugin usages.
+   * Set events in plugin and initialize metadata.
+   * @param {Function} [events.SelectEvent.next] - Function to emit selection event, use by the
+   * drawer.
+   * @param {Function} [events.EditEvent.next] - Function to emit edit event, use by the drawer.
+   * @param {Function} [events.DeleteEvent.next] - Function to emit delete event, use by the drawer.
+   */
+  init(events) {
+    this.__drawer.setEvents(events);
+    this.__metadata.parse();
+  }
+
+  /**
+   * Set resources in plugin.
+   * @param {Object} [resources=null] - Object that contains resources.
+   */
+  initResources(resources) {
+    this.__drawer.resources = resources;
+  }
+
+  /**
+   * Draws all data in the html element defined by the id.
+   * @param {String} id - Html id, without '#'.
+   */
+  draw(id) {
+    this.__drawer.draw(id);
+  }
+
+  /**
+   * Convert the content of files into plugin data.
+   * @param {FileInput[]} [inputs=[]] - File inputs you want to parse.
+   */
+  parse(inputs) {
+    this.__parser.parse(inputs);
+  }
+
+  /**
+   * Indicate if this parser can parse this file.
+   * @param {FileInformation} fileInformation - File information.
+   * @return {Boolean} - Boolean that indicates if this file can be parsed or not.
+   */
+  isParsable(fileInformation) {
+    return this.__parser.isParsable(fileInformation);
+  }
+
+  /**
+   * Return all generated files from plugin data.
+   * @return {FileInput[]} All generated files.
+   */
+  render() {
+    return this.__renderer.render();
+  }
+}
+
+export default DefaultPlugin;
---
+++ tests/unit/models/DefaultPlugin.spec.js
@@ -0,0 +1,137 @@
+import DefaultPlugin from 'src/models/DefaultPlugin';
+import DefaultData from 'src/models/DefaultData';
+import DefaultDrawer from 'src/draw/DefaultDrawer';
+import DefaultMetadata from 'src/metadata/DefaultMetadata';
+import DefaultParser from 'src/parser/DefaultParser';
+import DefaultRender from 'src/render/DefaultRender';
+
+describe('Test class: DefaultPlugin', () => {
+  describe('Test constructor', () => {
+    it('Check variables instantiation', () => {
+      const plugin = new DefaultPlugin();
+
+      expect(plugin.data).toEqual(new DefaultData());
+      expect(plugin.__drawer).toEqual(new DefaultDrawer(new DefaultData()));
+      expect(plugin.__metadata).toEqual(new DefaultMetadata(new DefaultData()));
+      expect(plugin.__parser).toEqual(new DefaultParser(new DefaultData()));
+      expect(plugin.__renderer).toEqual(new DefaultRender(new DefaultData()));
+    });
+
+    it('Check passing undefined variables to constructor', () => {
+      const plugin = new DefaultPlugin({});
+
+      expect(plugin.data).toEqual(new DefaultData());
+      expect(plugin.__drawer).toEqual(new DefaultDrawer(new DefaultData()));
+      expect(plugin.__metadata).toEqual(new DefaultMetadata(new DefaultData()));
+      expect(plugin.__parser).toEqual(new DefaultParser(new DefaultData()));
+      expect(plugin.__renderer).toEqual(new DefaultRender(new DefaultData()));
+    });
+
+    it('Check passing all variables to constructor', () => {
+      const plugin = new DefaultPlugin({
+        pluginData: 1,
+        pluginDrawer: 2,
+        pluginMetadata: 3,
+        pluginParser: 4,
+        pluginRenderer: 5,
+      });
+
+      expect(plugin.data).toEqual(1);
+      expect(plugin.__drawer).toEqual(2);
+      expect(plugin.__metadata).toEqual(3);
+      expect(plugin.__parser).toEqual(4);
+      expect(plugin.__renderer).toEqual(5);
+    });
+  });
+
+  describe('Test methods', () => {
+    describe('Test method: init', () => {
+      it('Should set events and call validate and parse of metadata', () => {
+        const mockSetEvents = jest.fn();
+        const mockParse = jest.fn();
+        const plugin = new DefaultPlugin({
+          pluginDrawer: {
+            setEvents: mockSetEvents,
+          },
+          pluginMetadata: {
+            parse: mockParse,
+          },
+        });
+
+        plugin.init();
+        expect(mockSetEvents).toBeCalled();
+        expect(mockParse).toBeCalled();
+      });
+    });
+
+    describe('Test method: initResources', () => {
+      it('Should set resources on drawer', () => {
+        const plugin = new DefaultPlugin({
+          pluginDrawer: {
+            resources: {},
+          },
+        });
+
+        plugin.initResources({ test: 'test' });
+        expect(plugin.__drawer.resources).toEqual({ test: 'test' });
+      });
+    });
+
+    describe('Test method: isParsable', () => {
+      it('Should call isParsable from parser', () => {
+        const mockIsParsable = jest.fn();
+        const plugin = new DefaultPlugin({
+          pluginParser: {
+            isParsable: mockIsParsable,
+          },
+        });
+
+        plugin.isParsable();
+        expect(mockIsParsable).toBeCalled();
+      });
+    });
+
+    describe('Test method: draw', () => {
+      it('Should call draw method from drawer', () => {
+        const mockDraw = jest.fn();
+        const plugin = new DefaultPlugin({
+          pluginDrawer: {
+            draw: mockDraw,
+          },
+        });
+
+        plugin.draw();
+        expect(mockDraw).toBeCalled();
+      });
+    });
+
+    describe('Test method: parse', () => {
+      it('Should call parse method from parser', () => {
+        const mockParse = jest.fn();
+        const plugin = new DefaultPlugin({
+          pluginParser: {
+            parse: mockParse,
+          },
+        });
+
+        plugin.parse();
+        expect(mockParse).toBeCalled();
+      });
+    });
+
+    describe('Test method: render', () => {
+      it('Should call render method from renderer', () => {
+        const mockRender = jest.fn(() => 'test');
+        const plugin = new DefaultPlugin({
+          pluginRenderer: {
+            render: mockRender,
+          },
+        });
+
+        const result = plugin.render();
+        expect(result).toEqual('test');
+        expect(mockRender).toBeCalled();
+      });
+    });
+  });
+});
