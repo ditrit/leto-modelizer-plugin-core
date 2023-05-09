e2a2f1967d0267d5bd46d26d7ce095fa1a3eb651
Vincent Moittié <moittie.vincent@gmail.com>
2023-05-09 10:59:10 +0200

    Update demo

--- demo/src/DemoPlugin.js
+++ demo/src/DemoPlugin.js
@@ -1,11 +1,13 @@
-import { DefaultData, DefaultPlugin } from 'leto-modelizer-plugin-core';
+import { DefaultConfiguration, DefaultData, DefaultPlugin } from 'leto-modelizer-plugin-core';
 import DemoMetadata from './DemoMetadata';
 import DemoParser from './DemoParser';
 
 class DemoPlugin extends DefaultPlugin {
   constructor(next) {
-    const data = new DefaultData({ name: 'demo' }, { next });
+    const configuration = new DefaultConfiguration();
+    const data = new DefaultData(configuration, { name: 'demo' }, { next });
     super({
+      configuration,
       pluginData: data,
       pluginMetadata: new DemoMetadata(data),
       pluginParser: new DemoParser(data),
