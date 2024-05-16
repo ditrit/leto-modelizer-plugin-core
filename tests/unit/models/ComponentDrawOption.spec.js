c5c98e28e57122f9a8324644d26a0def413b21d2
Vincent Moittié <moittie.vincent@gmail.com>
2024-05-16 14:50:35 +0200

    Remove useless properties in ComponentDrawOption and add hide property

--- tests/unit/models/ComponentDrawOption.spec.js
+++ tests/unit/models/ComponentDrawOption.spec.js
@@ -9,9 +9,7 @@ describe('Test class: ComponentDrawOption', () => {
       expect(componentDrawOption.y).toBeNull();
       expect(componentDrawOption.width).toBeNull();
       expect(componentDrawOption.height).toBeNull();
-      expect(componentDrawOption.needsResizing).toEqual(false);
-      expect(componentDrawOption.needsPositioning).toEqual(false);
-      expect(componentDrawOption.manuallyResized).toEqual(false);
+      expect(componentDrawOption.hide).toEqual(false);
     });
     it('Check passing undefined variables to constructor', () => {
       const componentDrawOption = new ComponentDrawOption({});
@@ -20,9 +18,7 @@ describe('Test class: ComponentDrawOption', () => {
       expect(componentDrawOption.y).toBeNull();
       expect(componentDrawOption.width).toBeNull();
       expect(componentDrawOption.height).toBeNull();
-      expect(componentDrawOption.needsResizing).toEqual(false);
-      expect(componentDrawOption.needsPositioning).toEqual(false);
-      expect(componentDrawOption.manuallyResized).toEqual(false);
+      expect(componentDrawOption.hide).toEqual(false);
     });
 
     it('Check passing variable to constructor', () => {
@@ -31,18 +27,14 @@ describe('Test class: ComponentDrawOption', () => {
         y: 2,
         width: 3,
         height: 4,
-        needsResizing: true,
-        needsPositioning: true,
-        manuallyResized: true,
+        hide: true,
       });
 
       expect(componentDrawOption.x).toEqual(1);
       expect(componentDrawOption.y).toEqual(2);
       expect(componentDrawOption.width).toEqual(3);
       expect(componentDrawOption.height).toEqual(4);
-      expect(componentDrawOption.needsResizing).toEqual(true);
-      expect(componentDrawOption.needsPositioning).toEqual(true);
-      expect(componentDrawOption.manuallyResized).toEqual(true);
+      expect(componentDrawOption.hide).toEqual(true);
     });
   });
 });
