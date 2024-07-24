fc5c68cd2250f0c6031459229bc995cd06ce12d3
Vincent Moittie <moittie.vincent@gmail.com>
2024-07-24 10:58:21 +0200

    Improve error management in ComponentAttribute

--- src/models/ComponentAttribute.js
+++ src/models/ComponentAttribute.js
@@ -1,3 +1,5 @@
+import ParserLog from './ParserLog';
+
 /**
  * Class to add Attribute in Components.
  */
@@ -8,7 +10,8 @@ class ComponentAttribute {
    * @param {string} [props.name] - The name of the attribute.
    * @param {(string | boolean | number | Array | ComponentAttribute[])} [props.value] - The
    * value of the attribute.
-   * @param {string} [props.type] - The real type of the attribute.
+   * @param {string} [props.type] - The real type of the attribute, valid types are
+   * String/Boolean/Number/Array/Object.
    * @param {ComponentAttributeDefinition} [props.definition] - The definition of the
    * attribute.
    */
@@ -57,96 +60,228 @@ class ComponentAttribute {
   }
 
   /**
-   * Check if the attribute has an error.
-   * @returns {boolean} - true if an error occurs otherwise false.
+   * Get all errors of the attribute.
+   * @param {ParserLog[]} [errors] - Errors to set, can be null.
+   * @param {boolean} [recurse] - If true, get sub-attributes errors recursively.
+   * @param {string} [id] - Component id.
+   * @returns {ParserLog[]} All attributes error.
    */
-  hasError() {
+  getErrors(errors = [], recurse = false, id = null) {
+    this.validateDefinitionType(errors, id);
+    this.validateType(errors, id);
+    this.validateRequired(errors, id);
+    this.validateRuleMinMax(errors, id);
+    this.validateRuleValues(errors, id);
+    this.validateRuleRegex(errors, id);
+
+    if (recurse && this.definition.type === 'Object' && Array.isArray(this.value)) {
+      this.value.forEach((attribute) => attribute.getErrors(errors, true, id));
+    }
+
+    return errors;
+  }
+
+  /**
+   * Set if definition type is according to type.
+   * @param {ParserLog[]} [errors] - Errors to set, can be null.
+   * @param {string} [id] - Component id.
+   */
+  validateDefinitionType(errors, id) {
     if (this.definition === null) {
-      if (this.value === null) {
-        return true;
+      return;
+    }
+
+    if (this.definition.type === 'Link' && this.type !== 'Array') {
+      errors.push(new ParserLog({
+        componentId: id,
+        severity: ParserLog.SEVERITY_ERROR,
+        message: 'parser.default.error.invalidLinkType',
+        attribute: this.name,
+      }));
+
+      return;
+    }
+
+    if (this.definition.type === 'Reference' && this.type !== 'String') {
+      errors.push(new ParserLog({
+        componentId: id,
+        severity: ParserLog.SEVERITY_ERROR,
+        message: 'parser.default.error.invalidReferenceType',
+        attribute: this.name,
+      }));
+
+      return;
+    }
+
+    if (this.definition.type !== 'Reference'
+      && this.definition.type !== 'Link'
+      && this.definition?.type !== this.type) {
+      errors.push(new ParserLog({
+        componentId: id,
+        severity: ParserLog.SEVERITY_ERROR,
+        message: 'parser.default.error.invalidType',
+        attribute: this.name,
+      }));
+    }
+  }
+
+  /**
+   * Set if type is according to type of value.
+   * @param {ParserLog[]} [errors] - Errors to set, can be null.
+   * @param {string} [id] - Component id.
+   */
+  validateType(errors, id) {
+    if (this.value === null) {
+      return;
+    }
+
+    if (this.type === 'Boolean') {
+      if (typeof this.value !== 'boolean') {
+        errors.push(new ParserLog({
+          componentId: id,
+          severity: ParserLog.SEVERITY_ERROR,
+          message: 'parser.default.error.notBoolean',
+          attribute: this.name,
+        }));
       }
 
-      if (this.type.toLowerCase() === 'array') {
-        return !Array.isArray(this.value);
+      return;
+    }
+
+    if (this.type === 'String') {
+      if (typeof this.value !== 'string') {
+        errors.push(new ParserLog({
+          componentId: id,
+          severity: ParserLog.SEVERITY_ERROR,
+          message: 'parser.default.error.notString',
+          attribute: this.name,
+        }));
       }
 
-      // eslint-disable-next-line valid-typeof
-      return typeof this.value !== this.type.toLowerCase();
+      return;
     }
 
-    if (this.value === null || this.value === undefined) {
-      return false;
+    if (this.type === 'Number') {
+      if (typeof this.value !== 'number') {
+        errors.push(new ParserLog({
+          componentId: id,
+          severity: ParserLog.SEVERITY_ERROR,
+          message: 'parser.default.error.notNumber',
+          attribute: this.name,
+        }));
+      }
+
+      return;
     }
 
-    return this.__typeOfValueValidation()
-      || this.__ruleValueValidation()
-      || this.__ruleMinAndMaxValidation()
-      || this.__ruleRegexValidation();
+    if (!Array.isArray(this.value)) { // For Object and Array types
+      errors.push(new ParserLog({
+        componentId: id,
+        severity: ParserLog.SEVERITY_ERROR,
+        message: `parser.default.error.not${this.type}`,
+        attribute: this.name,
+      }));
+    }
   }
 
   /**
-   * Check if the attribute type matches one of the JavaScript types.
-   * @returns {boolean} - true if the type is not good otherwise false.
-   * @private
+   * Set error if is required and value is null.
+   * @param {ParserLog[]} [errors] - Errors to set, can be null.
+   * @param {string} [id] - Component id.
    */
-  __typeOfValueValidation() {
-    const type = this.type.toLowerCase();
-
-    if (this.definition.type === 'Link') {
-      return !Array.isArray(this.value) || this.value.some((link) => typeof link !== 'string');
+  validateRequired(errors, id) {
+    if (this.definition?.required && this.value === null) {
+      errors.push(new ParserLog({
+        componentId: id,
+        severity: ParserLog.SEVERITY_ERROR,
+        message: 'parser.default.error.required',
+        attribute: this.name,
+      }));
     }
-
-    if (type === 'array') {
-      return !Array.isArray(this.value);
-    }
-
-    // eslint-disable-next-line valid-typeof
-    return typeof this.value !== type;
   }
 
   /**
-   * Check if values rule include this attribute value.
-   * @returns {boolean} - true if values rule doesn't include this attribute value otherwise false.
-   * @private
+   * Set error if :
+   * - Value is a string or array and its length is less than the specify minimum length.
+   * - Value is a string or array and its length is greater than the specify maximum length.
+   * - Value is a number and its value is less than the specify minimum value.
+   * - Value is a number and its value is greater than the specify maximum value.
+   * @param {ParserLog[]} [errors] - Errors to set, can be null.
+   * @param {string} [id] - Component id.
    */
-  __ruleValueValidation() {
-    if (this.definition.rules.values && Array.isArray(this.value)) {
-      return !this.value.every((v) => this.definition.rules.values.includes(v));
+  validateRuleMinMax(errors, id) {
+    if (this.type === 'Boolean' || this.type === 'Object') {
+      return;
     }
 
-    return this.definition.rules.values
-      && !this.definition.rules.values.includes(this.value);
-  }
-
-  /**
-   * Check if the value or its length follows min and max rules.
-   * @returns {boolean} - true if the value is smaller than the "min" rule
-   * or bigger than the "max" rule otherwise false.
-   * @private
-   */
-  __ruleMinAndMaxValidation() {
     const value = typeof this.value === 'number' ? this.value : this.value.length;
 
-    if (this.definition.rules.min !== null && value < this.definition.rules.min) {
-      return true;
+    if (this.definition
+      && this.definition.rules.min !== null
+      && value < this.definition.rules.min) {
+      errors.push(new ParserLog({
+        componentId: id,
+        severity: ParserLog.SEVERITY_ERROR,
+        message: `parser.default.error.min${this.definition.type}`,
+        attribute: this.name,
+      }));
     }
 
-    return this.definition.rules.max !== null && value > this.definition.rules.max;
+    if (this.definition
+      && this.definition.rules.max !== null
+      && value > this.definition.rules.max) {
+      errors.push(new ParserLog({
+        componentId: id,
+        severity: ParserLog.SEVERITY_ERROR,
+        message: `parser.default.error.max${this.definition.type}`,
+        attribute: this.name,
+      }));
+    }
   }
 
   /**
-   * Check if the value matches the regex rule.
-   * @returns {boolean} - true if attribute's value doesn't match the regex rule otherwise false.
-   * @private
+   * Set error if value or all elements of value array is not included in the specify values.
+   * @param {ParserLog[]} [errors] - Errors to set, can be null.
+   * @param {string} [id] - Component id.
    */
-  __ruleRegexValidation() {
-    if (this.definition.rules.regex !== null) {
-      const regex = new RegExp(this.definition.rules.regex);
-
-      return !regex.test(this.value);
+  validateRuleValues(errors, id) {
+    if (!this.definition || this.definition.rules.values.length === 0) {
+      return;
     }
 
-    return false;
+    const isArray = Array.isArray(this.value);
+
+    if ((isArray && this.value.some((value) => !this.definition.rules.values.includes(value)))
+     || (!isArray && !this.definition.rules.values.includes(this.value))) {
+      errors.push(new ParserLog({
+        componentId: id,
+        severity: ParserLog.SEVERITY_ERROR,
+        message: 'parser.default.error.invalidValue',
+        attribute: this.name,
+      }));
+    }
+  }
+
+  /**
+   * Set error if value is not null and not match the specify regex.
+   * @param {ParserLog[]} [errors] - Errors to set, can be null.
+   * @param {string} [id] - Component id.
+   */
+  validateRuleRegex(errors, id) {
+    if (!this.definition?.rules.regex || this.value === null) {
+      return;
+    }
+
+    const regex = new RegExp(this.definition.rules.regex);
+
+    if (!regex.test(this.value)) {
+      errors.push(new ParserLog({
+        componentId: id,
+        severity: ParserLog.SEVERITY_ERROR,
+        message: this.definition.rules.regexMessage || 'parser.default.error.regex',
+        attribute: this.name,
+      }));
+    }
   }
 }
 export default ComponentAttribute;
